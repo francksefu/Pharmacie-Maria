3b9eed2fcdbb469935c04a4bb50c913863d098eb
Franck sefu <francksefu1998@gmail.com>
2024-08-03 09:47:06 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,7 +1,7 @@
 <?php
-    $db_username= 'root';
-    $db_password= '';
-    $db_name= 'PacientEau';
+    $db_username= 'u823710390_maria';
+    $db_password= 'kalunga1998@F';
+    $db_name= 'u823710390_maria';
     $db_host= 'localhost';
 
     $db= mysqli_connect ($db_host, $db_username, $db_password, $db_name )
