8816aa05b3ec651eb0896dd187eb992c5700ba25
Franck sefu <francksefu1998@gmail.com>
2024-08-03 10:23:54 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -7,9 +7,9 @@
     $db= mysqli_connect ($db_host, $db_username, $db_password, $db_name )
     or die('Ne peux pas se connecter a la base de donnee'); */
 
-    $db_username= 'root';
-    $db_password= '';
-    $db_name= 'PacientEau_saruti';
+    $db_username= 'u823710390_maria';
+    $db_password= 'kalunga1998@F';
+    $db_name= 'u823710390_maria';
     $db_host= 'localhost';
 
     $db= mysqli_connect ($db_host, $db_username, $db_password, $db_name )
