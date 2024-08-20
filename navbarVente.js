6539eff80ea40d91fdcceeadcd09f5be97561d55
Franck sefu <francksefu1998@gmail.com>
2024-08-20 09:40:51 +0200

    Update navbarVente.js

--- navbarVente.js
+++ navbarVente.js
@@ -52,7 +52,7 @@ function navbar() {
       </a>
       <ul class="dropdown-menu">
           <li><a class="dropdown-item" href="caisseOut.php">Listez les sorties</a></li>
-          <li><a class="dropdown-item" href="addCaisseOut.php">Ajoutez sortie</a></li>
+          <li><a class="dropdown-item" href="addCaisseout.php">Ajoutez sortie</a></li>
           <li><a class="dropdown-item" href="updateTaux.php">Modifier taux d echange</a></li>
           <li><a class="dropdown-item" href="perteOccaz.php">Listez pertes occasionnée</a></li>
           <li><a class="dropdown-item" href="addPerteOccaz.php">Ajouter perte occasionnée</a></li>
